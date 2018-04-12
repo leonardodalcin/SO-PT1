6c0819a5b87dec31183502c2ca9059c16cb372ba
Leonardo Dalcin <leonardodalcin@outlook.com>
2018-04-12 14:36:38 -0300

    Initial ommit

---
+++ cmake-build-debug/CMakeFiles/SO_PT1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SO_PT1.dir/main.c.o"
+  "SO_PT1.pdb"
+  "SO_PT1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SO_PT1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
